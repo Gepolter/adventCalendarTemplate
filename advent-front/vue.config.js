c423653933718fd0a0d06d510d0f3079eb9bd739
Gepolter <73522230+Gepolter@users.noreply.github.com>
2023-08-24 15:24:55 +0200

    deployment changes

--- advent-front/vue.config.js
+++ advent-front/vue.config.js
@@ -1,22 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  chainWebpack: config => {
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => ({
-        ...options,
-        compilerOptions: {
-          // treat any tag that starts with ion- as custom elements
-          //isCustomElement: tag => tag.startsWith('Selection')
-        }
-      }))
-      
-  },
-  /*
-  publicPath: process.env.NODE_ENV === 'production'
-    ?'mhBuildPawcessor'
-    : ''*/
+
 })
